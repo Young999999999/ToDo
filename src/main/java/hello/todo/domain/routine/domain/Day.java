5b4ea499017ecde729d7971c7bd92c16942e04a3
young999999999 <ssas146310@gmail.com>
2025-04-09 22:10:13 +0900

    chore: 루틴에서 주기(Cycle)엔티티를 일대다 연관관계에서 밸류(Day) 컬렉션으로 수정

---
+++ src/main/java/hello/todo/domain/routine/domain/Day.java
@@ -0,0 +1,5 @@
+package hello.todo.domain.routine.domain;
+
+public enum Day {
+    MON,TUE,WED,THU,FRI,SAT,SUN;
+}
--- src/main/java/hello/todo/domain/routine/domain/Routine.java
+++ src/main/java/hello/todo/domain/routine/domain/Routine.java
@@ -1,9 +1,7 @@
 package hello.todo.domain.routine.domain;
 
 import hello.todo.common.BaseTimeEntity;
-import hello.todo.domain.member.domain.Member;
 import hello.todo.domain.member.domain.exception.CycleNotFoundException;
-import hello.todo.domain.routine.presentation.dto.request.CreateRoutineReqDTO;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -11,8 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Entity
 @Getter
@@ -36,41 +33,24 @@ public class Routine extends BaseTimeEntity {
 
     private LocalDate endDate;
 
-
-    @OneToMany(mappedBy = "routine",cascade = CascadeType.ALL,orphanRemoval = true)
-    private List<Cycle> cycles = new ArrayList<>();
-
-    //양방향 원자화 메서드
-    public void addCycle(Cycle cycle){
-        cycles.add(cycle);
-        cycle.changeRoutine(this);
-    }
-
-    public void removeCycle(Long removeCycleId){
-        cycles.removeIf(cycle -> (cycle.getId().equals(removeCycleId)));
-    }
-
-    private void setCycles(List<Cycle> cycles){
-        //사이클이 없다면 예외처리
-        if(cycles.isEmpty()){
-            throw new CycleNotFoundException("주기가 존재하지 않습니다.");
-        }
-
-        cycles.forEach(cycle -> addCycle(cycle));
-    }
-
+    @ElementCollection
+    @CollectionTable(name = "day",joinColumns = @JoinColumn(name = "routine_id"))
+    @Enumerated(value = EnumType.STRING)
+    Set<Day> days = new HashSet<>();
 
     // 멤버 팩토리 메소드를
-    private Routine(String name, List<Cycle> cycles, LocalDate startDate, LocalDate endDate) {
+    private Routine(Long memberId,String name, Set<Day> days, LocalDate startDate, LocalDate endDate) {
+        this.memberId = memberId;
         this.name = name;
-        setCycles(cycles);
+        this.days = days;
+        //setCycles(cycles);
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
     //TODO: end가 start보다 더 앞선 경우 예외 보내줘야함
-    static public Routine of(CreateRoutineReqDTO dto){
-        return new Routine(dto.name(),dto.cycles(),dto.startDate(),dto.endDate());
+    static public Routine of(Long memberId,String name, Set<Day> days, LocalDate startDate, LocalDate endDate){
+        return new Routine(memberId,name,days,startDate,endDate);
     }
 }
 
