d5f372f8a4718736844c03149114d229965d66ab
Young999999999 <ssas146310@gmail.com>
2025-04-17 14:51:53 +0900

    feat: 브라우저의 favicon.ico 요청 무시

--- src/main/java/hello/todo/domain/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/hello/todo/domain/auth/security/JwtAuthenticationFilter.java
@@ -34,7 +34,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // 로그인 관련 요청은 JWT 검증을 하지 않음
         String requestPath = request.getRequestURI();
         log.info(requestPath);
-        if (requestPath.startsWith("/api/v1/login")) {
+        if (requestPath.startsWith("/api/v1/login")||requestPath.startsWith("/favicon.ico")) {
             filterChain.doFilter(request, response);
             return;
         }
