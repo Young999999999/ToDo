e595730e37799ccc6f0845800ab50079e954c2cb
young999999999 <ssas146310@gmail.com>
2025-06-20 20:58:23 +0900

    refactor: 예외 발생시 로그 출력하도록 수정

--- src/main/java/hello/todo/domain/common/exception/CustomExceptionHandler.java
+++ src/main/java/hello/todo/domain/common/exception/CustomExceptionHandler.java
@@ -1,29 +1,25 @@
 package hello.todo.domain.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
-
 @RestControllerAdvice
+@Slf4j
 public class CustomExceptionHandler {
-
     // 커스텀 예외 처리
     @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e){
+    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
+        log.error("커스텀 예외 발생", e);
         return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
     }
 
-
     // 기타 예외 처리
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponseEntity> handleCustomException(Exception e){
+    protected ResponseEntity<ErrorResponseEntity> handleCustomException(Exception e) {
+        log.error("알 수 없는 예외 발생", e);
         return ErrorResponseEntity.toResponseEntity(ErrorCode.UNKNOWN_ERROR);
     }
-
-
-
-
-
 }
